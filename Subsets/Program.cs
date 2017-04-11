7c67adbfeceaa1a2561f7551df30137b8a0011ee
Andrei Lucaci <a.lucaci@netmatch.ro>
2017-04-11 14:23:43 +0300

    First commit

---
+++ Subsets/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Subsets
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			var sbProcessor = new SubListProcessor();
+			var result = sbProcessor.Process(new[] {1, 2, 3, 4, 5}, 4);
+			Console.WriteLine(string.Join(",", result));
+		}
+	}
+}
---
+++ Subsets/SubListProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Subsets
+{
+	public class SubListProcessor
+	{
+		public IEnumerable<int> Process(IEnumerable<int> input, int divisor)
+		{
+			var inputArray = input.ToArray();
+			for (var i = (1 << inputArray.Length); --i != 0;)
+			{
+				var sublist = BitCombinations(i).Select(n => inputArray[n]).ToArray();
+				if (sublist.Length > 1 && IsValidList(sublist, divisor)) return sublist;
+			}
+			return Enumerable.Empty<int>();
+		}
+
+		private static IEnumerable<int> BitCombinations(int number)
+		{
+			for (var i = 0; number != 0; number /= 2, i++)
+				if ((number & 1) != 0) yield return i;
+		}
+
+		private static bool IsValidList(IReadOnlyCollection<int> input, int divisor)
+		{
+			return
+				input.Count > 1 &&
+				(from i in input
+					from j in input
+					where i < j
+					select new {i, j}).All(x => (x.i + x.j) % divisor == 0);
+		}
+	}
+}
